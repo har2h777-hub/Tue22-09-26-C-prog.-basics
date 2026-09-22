6ada9583cadd0538f11bc00c075e518374061205
har2h777-hub <har2h777@gmail.com>
2026-09-22 22:38:37 +0530

    Add files via upload

---
+++ AVGoftwoNO.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+int Number1,Number2,Average;
+clrscr();
+printf("Enter your number 1 :");
+scanf("%d",&Number1);
+printf("Enter number 2 :");
+scanf("%d",&Number2);
+Average=(Number1+Number2)/2;
+printf("Your average is:%d",Average);
+getch();
+}
